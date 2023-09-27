f64b6f467b970de443fdede3a73766d67224b5f4
INKYLIN2333 <1073587029@qq.com>
2023-09-27 23:33:20 +0800

    创建产品清单，初步构建为用户展示产品相关属性的功能

---
+++ SportsStore/Models/Repository/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models.Repository
+{
+    public class EFDbContext:DbContext
+    {
+        /* 将Product模型类型用于表示Products表中的行 */
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsStore/Models/Repository/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models.Repository
+{
+    public class Repository
+    {
+        private EFDbContext context = new EFDbContext();
+        
+        /* 返回从EFDbContext类中读取同名属性的结果 */
+        public IEnumerable<Product> Products {
+            get { return context.Products; }
+        }
+    }
+}

0456aa06c7b7d36275e46660a2876b56c3485a69
INKYLIN2333 <1073587029@qq.com>
2023-10-06 15:23:23 +0800

    添加产品分页

--- SportsStore/Pages/Listing.aspx.cs
+++ SportsStore/Pages/Listing.aspx.cs
@@ -12,13 +12,26 @@ namespace SportsStore.Pages
     public partial class Listing : System.Web.UI.Page
     {
         private Repository repo = new Repository();
+        private int pageSize = 4;   //分页
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
         protected IEnumerable<Product> GetProducts()
         {
-            return repo.Products;
+            return repo.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((CurrentPage - 1) * pageSize)
+                .Take(pageSize);
+        }
+        protected int CurrentPage
+        {
+            get
+            {
+                int page;
+                return int.TryParse(Request.QueryString["page"], out page) ? page : 1;
+                //flase返回1,true返回page
+            }
         }
     }
 }
